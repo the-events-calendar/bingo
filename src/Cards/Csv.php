c7095510af7de9536229d229a7d606634ad89ee0
Gary Kovar <gkovar@gmail.com>
2018-06-21 15:12:33 -0400

    sends csv, no redirect needed

--- src/Cards/Csv.php
+++ src/Cards/Csv.php
@@ -2,6 +2,8 @@
 
 namespace Tribe\Bingo\Cards;
 
+use League\Csv\Reader;
+use League\Csv\Writer;
 use Tribe\Bingo\Settings\Page;
 
 class Csv {
@@ -16,11 +18,24 @@ class Csv {
 			wp_die( 'whoops' );
 		}
 
+		$csv = Writer::createFromString('');
+
+		$csv->insertOne( [
+			't1','r1','i1','b1','e1',
+			't2','r2','i2','b2','e2',
+			't3','r3','i3','b3','e3',
+			't4','r4','i4','b4','e4',
+			't5','r5','i5','b5','e5',
+		] );
+
 		$card_count = 1;
 		while ( $card_count <= $_POST[ Page::OPTION_NAME ][ Page::QUANTITY ] ) {
-			$this->cards->make_card();
+			$csv->insertOne( $this->cards->make_card() );
 			$card_count ++;
 		}
+
+		$reader = Reader::createFromString($csv->getContent());
+		$reader->output('bingo.csv');
 
 	}
 }
